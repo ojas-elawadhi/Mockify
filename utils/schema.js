76f45c8481594bf35545eb74a56510f8bc0dc541
Ojas Elawadhi <ojaselawadhi1@gmail.com>
2025-03-25 00:49:18 +0530

    feat: feedback page after interview

--- utils/schema.js
+++ utils/schema.js
@@ -11,7 +11,7 @@ export const MockInterview = pgTable("mockInterview", {
   mockId: varchar("mockId").notNull(),
 });
 
-export const userAnswer = pgTable("userAnswer", {
+export const UserAnswer = pgTable("userAnswer", {
   id: serial("id").primaryKey(),
   mockIdRef: varchar("mockIdRef").notNull(),
   question: varchar("question").notNull(),
