d318c089711c7bb2ce5a2626e03ce83c37f7b105
Ojas Elawadhi <ojaselawadhi1@gmail.com>
2025-04-01 00:37:52 +0530

    feat: test pricing stripe integration

---
+++ app/dashboard/_components/PricingPlan.jsx
@@ -0,0 +1,14 @@
+export default [
+  {
+    link: "https://buy.stripe.com/test_9AQ4jI8fffk75igcMN",
+    price: 7.99,
+    priceId: "price_1PUNYmSISOkj08jR30L0UpyY",
+    duration: "Monthly",
+  },
+  {
+    link: "https://buy.stripe.com/test_6oE6pA8nH66K1qgcMN",
+    price: 49.0,
+    priceId: "price_1R8e2qPQOw9EpV6rXoEGRvjU",
+    duration: "Yearly",
+  },
+];
